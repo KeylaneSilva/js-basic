2e05f9a3c3b19e48ce1ec4ebec54510ac38007f6
Keylane <keylanessilva07@gmail.com>
2021-05-01 02:32:37 -0300

    var-const-tp

---
+++ var-const-tp/script.js
@@ -0,0 +1,22 @@
+//variáveis
+let x = 21; //valores alteraveis
+let nome = 'Keylane';
+console.log("Meu nome é " + nome + " e tenho " + x + " anos");
+
+console.log(typeof (nome)); //exibe o tipo de variável
+
+//constantes
+
+const n1 = 3; //valor não alteraveis
+console.log(n1);
+
+//tipos primitivos
+
+let nome1 = 'Ricardo'; //string literal
+let idade = 21; //number literal
+let aprovado = true; //boolean
+let situacao = undefined; //undefined
+let cor = null; //usada para limpar algum campo
+
+
+
