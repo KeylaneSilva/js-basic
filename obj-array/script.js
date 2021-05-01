b42114e081ec00ad65a6a7014ac6409532207d8a
Keylane <keylanessilva07@gmail.com>
2021-05-01 02:31:52 -0300

    /

---
+++ obj-array/script.js
@@ -0,0 +1,33 @@
+//Objetos
+
+const nomePrincipal = 'Keylane';
+const idadePrincipal = 21;
+var data = new Date();
+
+const pessoas = {
+    nomex: nomePrincipal,
+    idadex: idadePrincipal,
+    hora: data.getDate()
+};
+
+console.log(pessoas);
+
+/*
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'da Silva'
+};
+
+console.log(pessoa);
+
+//Array - conjunto de dados que podem ser acessados por um index
+
+let familia = [true, 45, 'Ricardo', 10];
+console.log(familia.length);
+console.log(familia[0]);
+*/
+
+
